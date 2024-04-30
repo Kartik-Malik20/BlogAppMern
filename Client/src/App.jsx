adb00e82bb0f2817137584511e6ea41ce0d97220
Kartik malik <sanjaykartik2222@gmail.com>
2024-04-30 19:59:11 +0530

    Created pages jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Dashboard from './Pages/Dashboard'
+import SignIn from './Pages/SignIn'
+import Login from './Pages/Login'
+import Projects from './Pages/Projects'
 
 export default function App() {
   return (
-    <div className='text-2xl'>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/signup' element={<SignIn />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
