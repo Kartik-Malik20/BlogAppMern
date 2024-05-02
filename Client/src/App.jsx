1b5891a5b1f953bce5689f94c7aa42b03f194114
Kartik malik <sanjaykartik2222@gmail.com>
2024-05-02 20:29:20 +0530

    private route for admins only

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,30 +1,35 @@
-import React from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Dashboard from './Pages/Dashboard'
-import SignIn from './Pages/SignIn'
-import Login from './Pages/Login'
-import Projects from './Pages/Projects'
-import Header from './Components/Header'
-import BlogFooter from './Components/BlogFooter'
-import PrivateRoute from './Components/PrivateRoute'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Dashboard from "./Pages/Dashboard";
+import SignIn from "./Pages/SignIn";
+import Login from "./Pages/Login";
+import Projects from "./Pages/Projects";
+import Header from "./Components/Header";
+import BlogFooter from "./Components/BlogFooter";
+import PrivateRoute from "./Components/PrivateRoute";
+import CreatePost from "./Pages/CreatePost";
+import OnlyAdminPrivateRoute from "./Components/OnlyAdminPrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/projects" element={<Projects />} />
         <Route element={<PrivateRoute />}>
-        <Route path='/dashboard' element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/projects' element={<Projects />} />
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path="/create-post" element={<CreatePost />} />
+        </Route>
       </Routes>
       <BlogFooter />
     </BrowserRouter>
-  )
+  );
 }
--- Client/src/Components/DashProfile.jsx
+++ Client/src/Components/DashProfile.jsx
@@ -28,9 +28,10 @@ import {
   signoutSuccess,
 } from "../redux/user/userSlice";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
+import {Link} from 'react-router-dom'
 
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error,loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const filePickerRef = useRef();
@@ -165,8 +166,8 @@ export default function DashProfile() {
       const data = await res.json();
       if (!res.ok) {
         console.log(data.message);
-      }else{
-        dispatch(signoutSuccess())
+      } else {
+        dispatch(signoutSuccess());
       }
     } catch (error) {
       console.log(error.message);
@@ -252,9 +253,21 @@ export default function DashProfile() {
           type="submit"
           gradientDuoTone="purpleToBlue"
           className="uppercase"
+          disabled={loading || imageFileUploaded}
         >
-          Update
+          {loading ? 'loading...' : 'update'}
         </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type="button"
+              gradientDuoTone="purpleToPink"
+              className="uppercase w-full"
+            >
+              Create A Post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span onClick={() => setShowModal(true)} className="cursor-pointer">
