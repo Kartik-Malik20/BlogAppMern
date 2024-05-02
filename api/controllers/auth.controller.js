166032bf36c4eb3863d954310280f3dcbdae5d34
Kartik malik <sanjaykartik2222@gmail.com>
2024-05-02 21:22:38 +0530

    created api post route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -69,7 +69,7 @@ export const google = async(req,res,next) =>{
             const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
             const hashpassword = bcrypt.hashSync(generatedPassword, 10)
             const newUser = new User({
-                username:name.toLowerCase().split(' ').join(' ') + Math.random().toString(9).slice(-4),
+                username:name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4),
                 email,
                 password:hashpassword,
                 profilePicture:googlePhotoUrl,
