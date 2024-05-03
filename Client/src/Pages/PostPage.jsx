325e53a45c6ae7e9e22606a448c57e4953c8c786
Kartik malik <sanjaykartik2222@gmail.com>
2024-05-03 16:43:54 +0530

    Added call to action to post page

--- Client/src/Pages/PostPage.jsx
+++ Client/src/Pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Button, Spinner } from "flowbite-react";
+import CallToAction from "../Components/CallToAction";
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -58,12 +59,15 @@ export default function PostPage() {
           {post && post.category}
         </Button>
       </Link>
-      <img src={post && post.image} alt={post && post.title} className="mt-10 p-3 max-h-[600px] w-full object-fill" />
+      <img src={post && post.image} alt={post && post.title} className="mt-10 p-3 max-h-[600px] w-full object-contain" />
       <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.updatedAt).toLocaleDateString()}</span>
         <span className="italic">{post && (post.content.length/1000).toFixed(0)} mins read</span>
       </div>
       <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post && post.content}}></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
+      </div>
     </main>
   );
 }
