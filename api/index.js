583cf7851f3f18817ffd6379b7ebc8c794634c01
Kartik malik <sanjaykartik2222@gmail.com>
2024-06-09 13:26:15 +0530

    add pre installation for render

--- api/index.js
+++ api/index.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
 import postRoutes from "./routes/post.route.js";
 import commentRoute from "./routes/comment.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -19,9 +20,10 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(cors());
-
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,8 +34,14 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use('/api/post', postRoutes);
-app.use('/api/comment', commentRoute);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoute);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
